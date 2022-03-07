a11c970d0b790bdb260598efe9251e5acb5b3ece
Jared <jlhechanova@gmail.com>
2022-03-08 01:35:31 +0800

    modified to use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    return render(request, 'polls/index.html', {
-        'latest_question_list': Question.objects.order_by('-pub_date')[:5]
-    })
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # generic generates a context object name, hence the need to override it with our own.
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    return render(request, 'polls/detail.html', {
-        'question': get_object_or_404(Question, pk=question_id)
-    })
+    def get_queryset(self):
+        """Returns the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    return render(request, 'polls/results.html', {
-        'question': get_object_or_404(Question, pk=question_id)
-    })
+# def index(request):
+#     return render(request, 'polls/index.html', {
+#         'latest_question_list': Question.objects.order_by('-pub_date')[:5]
+#     })
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+# def detail(request, question_id):
+#     return render(request, 'polls/detail.html', {
+#         'question': get_object_or_404(Question, pk=question_id)
+#     })
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# def results(request, question_id):
+#     return render(request, 'polls/results.html', {
+#         'question': get_object_or_404(Question, pk=question_id)
+#     })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
