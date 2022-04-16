acfdcae54bc0340d7174ec9379de80053150fdeb
Kyle <kmbugayong@up.edu.ph>
2022-04-16 21:40:35 +0800

    UI changes in index

--- gspt_test/urls.py
+++ gspt_test/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 app_name = 'gspt_test'
 urlpatterns = [
     path('', views.index, name = 'index'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
