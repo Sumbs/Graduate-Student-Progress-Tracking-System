279a84f4dc9f1ed575b6dbe9ec1e6c63bbbc2f3b
KimTuico <kvtuico@up.edu.ph>
2022-05-09 19:50:44 +0800

    added user login

--- gspt_test/urls.py
+++ gspt_test/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 app_name = 'gspt_test'
@@ -8,4 +8,8 @@ urlpatterns = [
     path('students', views.students, name = 'students'),
     path('students/<int:person_id>/', views.study_plan, name = 'study_plan'),
     path('checklist', views.checklist, name = 'checklist'),
+    path('login', views.loginUser, name = 'login'),
+    path('logout', views.logoutUser, name = 'logout'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- gspt_test/views.py
+++ gspt_test/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 # Create your views here.
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('gspt_test:index')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            return redirect('gspt_test:index')
+        else:
+            messages.warning(request, 'Username or Password is incorrect.')
 
+    context = {}
+    return render(request, 'gspt_test/login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('gspt_test:login')
+
+@login_required(login_url='gspt_test:login')
 def index(request):
     return render(request, 'gspt_test/index.html')
 
+@login_required(login_url='gspt_test:login')
 def students(request):
     people = Person.objects.all()
     context = {"people": people}
 
     return render(request, 'gspt_test/students.html', context)
 
+@login_required(login_url='gspt_test:login')
 def study_plan(request, person_id):
     enrollments = (Enrollment
                     .objects.select_related('course_id')
@@ -23,5 +52,6 @@ def study_plan(request, person_id):
 
     return render(request, 'gspt_test/study_plan.html', context)
 
+@login_required(login_url='gspt_test:login')
 def checklist(request):
     return render(request, 'gspt_test/checklist.html')
