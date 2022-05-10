237de8dc981bb43311fb2eb016e015b64a002c7a
KimTuico <kvtuico@up.edu.ph>
2022-05-10 13:40:52 +0800

    implemented changing of password

--- gspt_test/views.py
+++ gspt_test/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from .models import *
 
 # Create your views here.
-
 def loginUser(request):
     if request.user.is_authenticated:
         return redirect('gspt_test:index')
@@ -14,7 +18,11 @@ def loginUser(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        user = authenticate(request, username=username, password=password)
+        try:
+            user = authenticate(request, username=User.objects.get(email=username), password=password)
+        except:
+            user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request,user)
             if 'next' in request.POST:
@@ -39,7 +47,6 @@ def index(request):
 def students(request):
     people = Person.objects.all()
     context = {"people": people}
-
     return render(request, "gspt_test/students.html", context)
 
 
@@ -63,3 +70,11 @@ def checklist(request):
 @login_required(login_url='gspt_test:login')
 def home(request):
     return render(request, "gspt_test/home.html")
+
+class change_password(LoginRequiredMixin, SuccessMessageMixin, PasswordChangeView):
+    login_url = 'gspt_test:login'
+    redirect_field_name = 'next'
+    template_name = 'gspt_test/change_password.html'
+    success_url = reverse_lazy('gspt_test:home')
+    success_message = 'Password Changed Succesfully!'
+
